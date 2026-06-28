1d1eee585cd1ad6b85f2c18e323ca9d464f03fde
Abhishek Bhat <your_email@example.com>
2026-06-28 17:11:37 +0530

    Fix API base URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://finance-tracker-3-kamm.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
